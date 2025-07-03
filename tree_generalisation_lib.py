f2eeb0dc0ba6c86f55905ca6ef37196829e65d32
artie2000 <artem.khovanov@gmail.com>
2025-07-03 03:29:48 +0100

    make random dropout work

--- tree_generalisation_lib.py
+++ tree_generalisation_lib.py
@@ -62,16 +62,68 @@ def get_tree_vars(tree):
         return {tree["var"]}
     return get_tree_vars(tree["left"]) | get_tree_vars(tree["right"])
 
-def gen_tree(depth = 0, depth_probs = [1,0.8,0.6,0.4,0.2,0.1,0], var_depth_mult = 0.5):
-    if random.random() < depth_probs[depth]:
-        return {"var" : None,
-                "left" : gen_tree(depth + 1, depth_probs=depth_probs,var_depth_mult=var_depth_mult),
-                "right" : gen_tree(depth + 1, depth_probs=depth_probs,var_depth_mult=var_depth_mult)}
-    else:
-        k = 0
-        while random.random() < var_depth_mult:
-            k += 1
-        return {"var" : k, "left" : None, "right" : None}
+def geom(p):
+    k = 0
+    while random.random() < p:
+        k += 1
+    return k
+
+def bal_brackets_to_tree(text, var_depth_mult = 0.5):
+    if text == "": return {"var" : geom(var_depth_mult), "left" : None, "right" : None}
+
+    b = 0
+    key_pos = None
+    for i in range(len(text)):
+        c = text[i]
+        if c == "(":
+            b += 1
+        elif c == ")":
+            b -= 1
+        if b == 0:
+            key_pos = i
+            break
+
+    return {"var" : None,
+            "left" : bal_brackets_to_tree(text[1:key_pos],var_depth_mult=var_depth_mult),
+            "right" : bal_brackets_to_tree(text[key_pos+1:],var_depth_mult=var_depth_mult)}
+
+# generate a uniformly random binary tree with n non-leaf nodes
+# variable labels are generated with a geometric distibution, parameter var_depth_mult
+def gen_tree_with_nodes(n, var_depth_mult = 0.5):
+    left_set = set(random.sample(range(2*n), n))
+    brackets = "".join(["(" if i in left_set else ")" for i in range(2*n)])
+
+    i = 0
+    start_idx = 0
+    end_idx = 2*n
+    bal = 0
+
+    while i < end_idx:
+        if brackets[i] == "(": bal += 1
+        else: bal -= 1
+
+        if bal == 0:
+            if brackets[start_idx] == "(":
+                start_idx = i + 1
+            else:
+                brackets = (
+                    brackets[:start_idx] + 
+                    "(" + brackets[i+1:end_idx] + ")" + 
+                    "".join([")" if brackets[j] == "(" else "(" for j in range(start_idx+1,i)]) +
+                    brackets[end_idx:])
+                end_idx = end_idx - i + start_idx
+                i = start_idx
+                start_idx += 1
+        
+        i += 1
+    
+    return bal_brackets_to_tree(brackets, var_depth_mult=var_depth_mult)
+
+def gen_tree(n=None,n_min=2,n_max=10):
+    if n == None:
+        n = random.randrange(n_min, n_max+1)
+    return gen_tree_with_nodes(n-1)
+
 
 # substitute base using the given variable -> tree dict
 def substitute(base, vars):
@@ -102,15 +154,21 @@ def get_substitution(f, g):
 
     return False, None
 
+# return: tree, substitutions, indices of correct substitutions
 def gen_tree_with_subs(k = 10, prop_correct = 1):
-    tree = gen_tree()
+    n = n = random.randrange(2, 21)
+    tree = gen_tree(n)
     vars = get_tree_vars(tree)
     num_correct = int(k * prop_correct)
-    derived = [substitute(tree, {var: gen_tree(depth_probs = [0.75,0.5,0.25,0]) for var in vars})
+    derived = [substitute(tree, {var: gen_tree(n_min=1,n_max=7) for var in vars})
                for _ in range(num_correct)]
-    derived.extend([gen_tree() for _ in range(k - num_correct)])
-    random.shuffle(derived)
-    return tree, derived
+    derived.extend([gen_tree(n_min=2*n,n_max=5*n) for _ in range(k - num_correct)])
+
+    # shuffle
+    indices = list(range(k))
+    random.shuffle(indices)
+    derived = [derived[indices[i]] for i in range(k)]
+    return tree, derived, {i for i in range(k) if indices[i] < num_correct}
 
 def gen_text(tree, derived):
     text_start = "^" + "".join([tree_to_text(der) + "," for der in derived])[:-1] + ";"
@@ -118,15 +176,15 @@ def gen_text(tree, derived):
     return text_start, text_full
 
 # parse and check (see gen_text for format)
-# tree: the base tree, derived: the substitutions of the base tree
-def check_generalisation(tree, derived, text_full):
+# tree: the base tree, derived: the substitutions of the base tree, correct_inds: the indices to check
+def check_generalisation(tree, derived, correct_inds, text_full):
     try:
         gen = text_to_tree(text_full[1:-1].split(";")[1])
     except Exception as e: # parsing error
         return False, str(e)
 
     if get_substitution(tree, gen)[0]:
-        if all([get_substitution(gen, der)[0] for der in derived]):
+        if all([get_substitution(gen, derived[i])[0] for i in range(len(derived)) if i in correct_inds]):
             return True, None
         else:
             return False, "Not a generalisation of the derived trees"
@@ -135,7 +193,8 @@ def check_generalisation(tree, derived, text_full):
 
 def gen_train_tokens():
     while True:
-        tree, derived = gen_tree_with_subs()
+        prop_correct = random.uniform(0.5,1)
+        tree, derived, _ = gen_tree_with_subs(prop_correct = prop_correct)
         _, text_full = gen_text(tree, derived)
 
         if hash_tree(tree) != 7:
@@ -145,21 +204,21 @@ def gen_train_tokens():
 
 def gen_eval_problem(prop_correct = 1):
     while True:
-        tree, derived = gen_tree_with_subs(prop_correct = prop_correct)
+        tree, derived, correct_inds = gen_tree_with_subs(prop_correct = prop_correct)
         text_start, _ = gen_text(tree, derived)
 
         if hash_tree(tree) == 7:
             break
 
-    return tree, derived, text_start
+    return tree, derived, correct_inds, text_start
 
 # model: function completing problem string -> solution string
 def eval_model(model, eval_iters=1000):
     count = 0
 
     for i in range(eval_iters):
-        tree, derived, text_start = gen_eval_problem()
-        if check_generalisation(tree, derived, detokenise(model(tokenise(text_start))))[0]:
+        tree, derived, correct_inds, text_start = gen_eval_problem(prop_correct = 0.6)
+        if check_generalisation(tree, derived, correct_inds, detokenise(model(tokenise(text_start))))[0]:
             count += 1
 
     return str(count)
