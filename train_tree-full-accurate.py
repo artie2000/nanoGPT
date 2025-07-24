3aac516afa503e4a40fa100f32c55757f44ee23f
artie2000 <artem.khovanov@gmail.com>
2025-07-24 06:40:25 +0100

    improve setup

--- train_tree-full-accurate.py
+++ train_tree-full-accurate.py
@@ -6,34 +6,34 @@ def data_gen_on_demand():
     while True:
         yield from gen_train_tokens(prop_correct_fn = lambda: 1)
 
-out_name = "tree"
+out_name = "tree-full-accurate"
 eval_fn = lambda model: eval_model(model, prop_correct = 1)
 eval_stop_token = ord("$")
 data_iter = data_gen_on_demand
-batch_size = 64
+batch_size = 32
 block_size = 768
-gradient_accumulation_steps = 1 * 8 # used to simulate larger batch sizes
+gradient_accumulation_steps = 1 * 16 # used to simulate larger batch sizes
 
 # model
-n_layer = 6
-n_head = 6
-n_embd = 384
+n_layer = 12
+n_head = 12
+n_embd = 768
 
 # eval stuff
-eval_interval = 400
-eval_iters = 200
-log_interval = 4
+eval_interval = 1000
+eval_iters = 1000
+log_interval = 100
 out_dir = 'out' # checkpointing
 
 # optimizer
 learning_rate = 0.002 # max learning rate
-max_iters = 18_000 # total number of training iterations
+max_iters = 100_000 # total number of training iterations
 weight_decay = 0.05
 beta1 = 0.9
 beta2 = 0.98
 
 # learning rate decay settings
 decay_lr = True # whether to decay the learning rate
-warmup_iters = 100 # how many steps to warm up for
-lr_decay_iters = 18_000 # should be ~= max_iters per Chinchilla
+warmup_iters = 1000 # how many steps to warm up for
+lr_decay_iters = 100_000 # should be ~= max_iters per Chinchilla
 min_lr = 0.0002 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
---
+++ train_tree-small-fuzzy.py
@@ -0,0 +1,40 @@
+# $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
+from tree_generalisation_lib import gen_train_tokens, eval_model
+import random
+
+# data
+def data_gen_on_demand():
+    while True:
+        yield from gen_train_tokens(prop_correct_fn = lambda: random.choice([0.5,0.6,0.7,0.8,0.9,1]))
+
+out_name = "tree-small-fuzzy"
+eval_fn = lambda model: eval_model(model, prop_correct = 0.6)
+eval_stop_token = ord("$")
+data_iter = data_gen_on_demand
+batch_size = 64
+block_size = 768
+gradient_accumulation_steps = 1 * 8 # used to simulate larger batch sizes
+
+# model
+n_layer = 6
+n_head = 6
+n_embd = 384
+
+# eval stuff
+eval_interval = 400
+eval_iters = 1000
+log_interval = 40
+out_dir = 'out' # checkpointing
+
+# optimizer
+learning_rate = 0.002 # max learning rate
+max_iters = 18_000 # total number of training iterations
+weight_decay = 0.05
+beta1 = 0.9
+beta2 = 0.98
+
+# learning rate decay settings
+decay_lr = True # whether to decay the learning rate
+warmup_iters = 100 # how many steps to warm up for
+lr_decay_iters = 18_000 # should be ~= max_iters per Chinchilla
+min_lr = 0.0002 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
